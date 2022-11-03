67448fe97d47e1a4fa4dd88797d83c091038f694
Ing. Nelson Gonzalez Escalante <nescalan@gmail.com>
2022-11-03 11:58:16 -0600

    new: [leccion-10] new file

---
+++ modulos/1-basico/php-basico/leccion-9.php
@@ -0,0 +1,10 @@
+<?php
+
+echo "Hola todos, estoy empezando con PHP \n";
+
+$name = "Nelson";
+$lastName ="Escalante";
+
+// echo " No, no. Yo me llamo ", $name . " " . $lastName . "\n" ;
+
+echo "Yo me llamo $name $lastName \n";
