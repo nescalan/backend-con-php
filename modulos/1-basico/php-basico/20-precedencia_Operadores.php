6da29cf693e492f3f2654cafe3fd95bf073e96d5
Ing. Nelson Gonzalez Escalante <nescalan@gmail.com>
2022-11-03 15:42:08 -0600

    new: [20-precedencia] new file

---
+++ modulos/1-basico/php-basico/20-precedencia_Operadores.php
@@ -0,0 +1,14 @@
+<?php
+
+$counter = 10;
+$newValue = ++$counter;
+$true = true;
+$false = false;
+$booleanos = ($true and $false);
+
+echo "This is counter $newValue";
+echo "\n";
+
+
+echo "This is really $booleanos";
+echo "\n";
