57d9359ff6eb245c6fb72be4a5823f99eaaae57e
Ing. Nelson Gonzalez Escalante <nescalan@gmail.com>
2022-11-04 11:24:41 -0600

    Lesson 5: Reto de arreglos

--- modulos/1-basico/2-php-basico/arrays/4-arrayFunctions.php
+++ modulos/1-basico/2-php-basico/arrays/4-arrayFunctions.php
@@ -1,7 +1,15 @@
 <?php
 
+// Declaracion: Variables
 $edades = [18, 22, 40, 34];
+$listaFrutas = "banano, sandia, papaya";
+$listaFrutasArreglo = [];
 
+// Script: Funciones para arreglos
 echo count($edades);
 echo "\n";
-// var_dump(is_array($edades));
+
+$listaFrutasArreglo = explode(",", $listaFrutas);
+var_dump($listaFrutasArreglo);
+
+
---
+++ modulos/1-basico/2-php-basico/arrays/5-reto.php
@@ -0,0 +1,27 @@
+ <?php
+//  nombre, ocupacion, color, comidas
+
+// Declaration: Arrays
+$firstChallenge = [
+    "alunmno" => [
+        "nombre" => "Nelson",
+        "ocupacion" => "Ingeniero",
+        "color" => "azul",
+        "comidas" => [
+            "favoritas" => "Mexicana",
+            "noFavoritas" => "India",
+        ]
+    ]
+    "alumno" => [
+        "nombre" => "Nelson",
+        "ocupacion" => "Ingeniero",
+        "color" => "azul",
+        "comidas" => [
+            "favoritas" => "Mexicana",
+            "noFavoritas" => "India",
+        ],
+    ],
+    ];
+
+    var_dump($firstChallenge);
+    echo "\n";
